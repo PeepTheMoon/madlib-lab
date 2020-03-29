b719f665a2b3e0debe113f9be4ba14d2b945be81
RACHEL DONAHUE <RACHEL.INGOODHEALTH@GMAIL.COM>
2020-03-29 00:47:23 -0700

    Added more styling in css

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ console.log('Howdy there!');
 // Get Dom Elements
 
 // get elements from html by id
-
 //get word inputs from user
 const noun1Input = document.getElementById('noun1-input');
 const noun1Span = document.getElementById('noun1-span');
@@ -11,8 +10,8 @@ const noun1Span = document.getElementById('noun1-span');
 const noun2Input = document.getElementById('noun2-input');
 const noun2Span = document.getElementById('noun2-span');
 
-const noun3Input = document.getElementById('noun3-input');
-const noun3Span = document.getElementById('noun3-span');
+const adj1Input = document.getElementById('adj1-input');
+const adj1Span = document.getElementById('adj1-span');
 
 const noun4Input = document.getElementById('noun4-input');
 const noun4Span = document.getElementById('noun4-span');
@@ -26,8 +25,8 @@ const noun6Span = document.getElementById('noun6-span');
 const noun7Input = document.getElementById('noun7-input');
 const noun7Span = document.getElementById('noun7-span');
 
-const noun8Input = document.getElementById('noun8-input');
-const noun8Span = document.getElementById('noun8-span');
+const adj2Input = document.getElementById('adj2-input');
+const adj2Span = document.getElementById('adj2-span');
 
 const verb1Input = document.getElementById('verb1-input');
 const verb1Span = document.getElementById('verb1-span');
@@ -55,14 +54,15 @@ const myButton = document.getElementById('button');
 function enterAnswers() {
     // this event listener should . . .
     // GET the value of the inputs
+
     const noun1InputValue = noun1Input.value;
     noun1Span.textContent = noun1InputValue;
 
     const noun2InputValue = noun2Input.value;
     noun2Span.textContent = noun2InputValue;
 
-    const noun3InputValue = noun3Input.value;
-    noun3Span.textContent = noun3InputValue;
+    const adj1InputValue = adj1Input.value;
+    adj1Span.textContent = adj1InputValue;
 
     const noun4InputValue = noun4Input.value;
     noun4Span.textContent = noun4InputValue;
@@ -76,8 +76,8 @@ function enterAnswers() {
     const noun7InputValue = noun7Input.value;
     noun7Span.textContent = noun7InputValue;
 
-    const noun8InputValue = noun8Input.value;
-    noun8Span.textContent = noun8InputValue;
+    const adj2InputValue = adj2Input.value;
+    adj2Span.textContent = adj2InputValue;
 
     const verb1InputValue = verb1Input.value;
     verb1Span.textContent = verb1InputValue;
@@ -92,10 +92,11 @@ function enterAnswers() {
     verb4Span.textContent = verb4InputValue;
 
     // use the input values to SET the value of our spans
-    console.log(noun1InputValue, noun2InputValue, noun3InputValue, noun4InputValue, verb1InputValue, verb2InputValue, noun5InputValue, noun6InputValue, verb3InputValue, verb4InputValue, noun7InputValue, noun8InputValue);
+    console.log(noun1InputValue, noun2InputValue, adj1InputValue, noun4InputValue, verb1InputValue, verb2InputValue, noun5InputValue, noun6InputValue, verb3InputValue, verb4InputValue, noun7InputValue, adj2InputValue);
     
     // hide the input container
     inputContainer.style.display = 'none';
+
     // reveal the mad lib container
     madlibContainer.style.display = 'block';
 }
